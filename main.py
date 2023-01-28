fc754ece8fb193d929aefb625dd1568bee047ad0
主 <120739758+ledges@users.noreply.github.com>
2023-01-28 07:02:24 -0500

    made the code look nice and added Session reqs.

--- main.py
+++ main.py
@@ -1,57 +1,52 @@
 import requests
 import random
-import os
-from colorama import Fore
-from pystyle import *
-
-def check():
-  os.system('cls')
-  print(Colorate.Horizontal(Colors.blue_to_purple, """
-  _   __          _ __         _______           __          
- | | / /__ ____  (_) /___ __  / ___/ /  ___ ____/ /_____ ____
- | |/ / _ `/ _ \/ / __/ // / / /__/ _ \/ -_) __/  '_/ -_) __/
- |___/\_,_/_//_/_/\__/\_, /  \___/_//_/\__/\__/_/\_\\__/_/   
-                     /___/                                   
 
 
- """, 1))
-  while True:
-    proxie = open('proxies.txt', 'r').read().splitlines()
-    prox = random.choice(proxie)
-    proxies = {'http://': f'http://{prox}', 'https://': f'http://{prox}'}
-    #invite = ''.join((random.choice('abcdefghijklmnopqrstuvwxyz1234567890')) for x in range(2))
-    vanitys = open("vanitys.txt")
-    invite = random.choice(vanitys.read().splitlines())
-    vanitys.close()
-    headers = {
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Cache-Control': 'max-age=0',
-      'Connection': 'keep-alive',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'none',
-      'Sec-Fetch-User': '?1',
-      'Upgrade-Insecure-Requests': '1',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-      'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Windows"',
-  }
-  
-    r = requests.get(f'https://discord.com/api/v9/invites/{invite}', headers=headers, proxies=proxies)
-    if r.status_code == 404:
-      print(Fore.RESET + Fore.GREEN + f"Available: {invite}")
-      with open("available.txt", "a") as f:
-        f.write("discord.gg/" + invite + "\n")
-
-      
-    if r.status_code == 200:
-      print(Fore.RESET + Fore.RED + f"Taken: {invite}") 
+def headers(token: str = ""):
+    return {
+        "authority": "discord.com",
+        "x-discord-locale": "en-US",
+        "x-debug-options": "bugReporterEnabled",
+        "accept-language": "en-US",
+        "authorization": token,
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9007 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36",
+        "accept": "*/*",
+        "origin": "https://discord.com",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+    }
 
 
+def check(vanity: str = "", token: str = ""):
+    proxy = open("./data/proxies.txt", "r").read().splitlines()
+    proxy = random.choice(proxy)
+    proxies = {
+      "http://": f"http://{proxy}", 
+      "https://": f"http://{proxy}"
+    }
+    with requests.Session() as session:
+        check_res = session.get(
+            f"https://discord.com/api/v9/invites/{vanity}",
+            headers=headers(token),
+            proxies=proxies,
+        ).text
+
+        if "vanity_url_code" in check_res:
+            pass
+
+        elif "Unknown Invite" in check_res:
+            print(f">> [{vanity}] available")
+            open("./data/available.txt", "a").write(f"{vanity}\n")
 
 
+def _():
+    while True:
+        ok = open("./data/vanitys.txt", "r").read().splitlines()
+        check(
+          vanity=random.choice(ok), 
+          token=""
+        )
 
 
-check()
+_()
